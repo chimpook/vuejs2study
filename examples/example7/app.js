302b5471baeaf8cce6a89aacc6e05c70dad89bbf
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-20 22:41:24 +0300

    30. Settind styles dynamically without CSS classes

---
+++ examples/example7/app.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(event) {
+    new Vue({
+        el: '#app',
+        data : {
+            color: 'green',
+            width: 100
+        },
+        computed: {
+            myStyle: function() {
+                return {
+                    backgroundColor: this.color,
+                    width: this.width + 'px'
+                };
+            }
+        }
+    });
+});
