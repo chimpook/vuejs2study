d621a305d368408074dc211a763680823a1c4381
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-22 00:33:57 +0300

    Exercise5. complete

--- exercises/exercise5/app.js
+++ exercises/exercise5/app.js
@@ -2,17 +2,26 @@ window.addEventListener("load", function(event) {
     new Vue({
         el: '#exercise',
         data: {
-          array: ['Max', 'Anna', 'Chris', 'Manu'],
-          myObject: {
-            title: 'Lord of the Rings',
-            author: 'J.R.R. Tolkiens',
-            books: '3'
-          },
-          testData: {
-            name: 'TESTOBJECT', 
-            id: 10,
-            data: [1.67, 1.33, 0.98, 2.21]
-          }
+            flag: true,
+            array: ['Max', 'Anna', 'Chris', 'Manu'],
+            myObject: {
+                title: 'Lord of the Rings',
+                author: 'J.R.R. Tolkiens',
+                books: '3'
+            },
+            testData: {
+                name: 'TESTOBJECT', 
+                id: 10,
+                data: [1.67, 1.33, 0.98, 2.21]
+            }
+        },
+        methods: {
+            toggle: function() {
+                this.flag = !this.flag;
+            },
+            test: function(object) {
+                return is_array(object);
+            }
         }
       });
 });
