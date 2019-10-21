eb970d0b1e74ede73192ebbfb722885b6f2d0ce6
Sergey Veselovskiy <chimpook@gmail.com>
2019-10-21 20:44:20 +0300

    39. Rendering lists with v-for

---
+++ examples/example9/app.js
@@ -0,0 +1,12 @@
+window.addEventListener("load", function(event) {
+    new Vue({
+        el: '#app',
+        data : {
+            ingredients: ['meat', 'fruit', 'cookies'],
+            persons: [
+                {name: 'Max', age: 27, color: 'red'},
+                {name: 'Anna', age: 'unknown', color: 'blue'}
+            ]
+        }
+    });
+});
