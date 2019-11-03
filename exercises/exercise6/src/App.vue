9d3891403fe41fc614608dca5e1c9cd2330612a9
Sergey Veselovskiy <chimpook@gmail.com>
2019-11-04 00:34:04 +0300

    v-bind:key added

--- exercises/exercise6/src/App.vue
+++ exercises/exercise6/src/App.vue
@@ -13,7 +13,7 @@
                 <ul class="list-group">
                     <li
                             class="list-group-item"
-                            v-for="index in 5">
+                            v-for="index in 5" v-bind:key="index">
                         Server #{{ index }}
                     </li>
                 </ul>
