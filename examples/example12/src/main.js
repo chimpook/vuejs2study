c0f56b14c931fcc1d28242956128617e302490ed
Sergey Veselovskiy <chimpook@gmail.com>
2019-11-17 20:08:20 +0300

    117. Centralizing Code in an Event Bus

--- examples/example12/src/main.js
+++ examples/example12/src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-export const eventBus = new Vue();
+export const eventBus = new Vue({
+  data: {
+    someData: 123
+  },
+  methods: {
+    changeAge(age) {
+      this.$emit('ageWasEdited', age);
+    }
+  }
+});
 
 new Vue({
   render: h => h(App),
