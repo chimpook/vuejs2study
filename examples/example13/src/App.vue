4e514d6a60c0c9c1ac9d718766cd63a0283effd5
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-11-21 19:13:22 +0300

    128. Default slots and Slot defaults

--- examples/example13/src/App.vue
+++ examples/example13/src/App.vue
@@ -3,8 +3,9 @@
     <div class="row">
       <div class="col-12">
         <app-quote>
+          <p>Here we are:</p>
           <h2 slot="title">{{ quoteTitle }}</h2>
-          <p slot="content">A Wonderful Quote!!</p>
+          <p>A Wonderful Quote!!</p>
         </app-quote>
       </div>
     </div>
