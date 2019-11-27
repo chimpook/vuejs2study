a144b143e9fe167faddcc41d9fc71918673d2c9f
Sergey Veselovskiy <chimpook@gmail.com>
2019-11-28 01:40:37 +0300

    Assignment9: forms

---
+++ exercises/10. forms/src/App.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="container">
+        <form v-if="!isSubmitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <h1>Signup form</h1>
+                    <hr>
+                    <app-full-name v-model="userData.full_name"></app-full-name>
+                    <!-- Mail -->
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" 
+                            class="form-control" v-model="userData.email">
+                    </div>
+                    <!-- Password -->
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" 
+                            class="form-control" v-model="userData.password">
+                    </div>
+                    <!-- Store Data? Yes/No -->
+                    <div class="form-group">
+                        <label for="storeData">
+                            <input
+                                    type="checkbox"
+                                    id="storeData"
+                                    value="true"
+                                    v-model="userData.storeData"> Store Data
+                        </label>
+                    </div>
+
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button
+                            class="btn btn-primary"
+                            @click.prevent="submitted">Submit!
+                    </button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="isSubmitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ userData.full_name }}</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password: {{ userData.password }}</p>
+                        <p>Store in Database?: {{ userData.storeData ? 'Yes' : 'No' }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import FullName from './components/FullName.vue';
+    export default {
+        data() {
+            return {
+                userData: {
+                    first_name: '',
+                    last_name: '',
+                    full_name: '',
+                    email: '',
+                    password: '',
+                    storeData: true
+                },
+                isSubmitted: false,
+                test: ''
+            }
+        },
+        methods: {
+            submitted() {
+                this.isSubmitted = true;
+            }
+        },
+        components: {
+            appFullName: FullName
+        }
+    }
+</script>
+
+<style>
+</style>
