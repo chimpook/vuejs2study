df548c8f604eb4c0f8273e2203f700747910d0f5
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-11-28 18:49:27 +0300

    Assignment10: directives

---
+++ exercises/11. directives/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-myon:click="buzz" class="btn btn-success">Click me</button>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                counter: 0
+            }
+        },
+        methods: {
+            buzz() {
+                this.counter++;
+                /* eslint-disable no-console */
+                console.log('Buzzz ' + this.counter + ' times..');
+                /* eslint-enable no-console */
+            }
+        },
+        directives: {
+            'myon': {
+                bind(el, binding) {
+                    if (binding.arg == 'click') {
+                        let fn = binding.value;
+                        if (typeof fn === 'function') {
+                            el.addEventListener('click', fn);
+                        }
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
