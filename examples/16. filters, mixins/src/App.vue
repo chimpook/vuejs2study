f2b42b40413342f42bf2e0b295bc61e6112c43e1
Sergey Veselovskiy <chimpook@gmail.com>
2019-11-29 13:06:07 +0300

    16. two components with filters. Code duplicated.

--- examples/16. filters, mixins/src/App.vue
+++ examples/16. filters, mixins/src/App.vue
@@ -4,21 +4,42 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | to-lowercase }}</p>
+                <hr>
+                <input type="text" v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+                </ul>
+                <hr>
+                <app-list></app-list>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    import List from './components/List.vue';
     export default {
         data() {
             return {
-                text: 'Hello kitty!'
+                text: 'Hello kitty!',
+                fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText: ''
             }
+        },
+        components: {
+            appList: List
         },
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
+            }
+        },
+        // better solution tham filter - recalculates only when changed
+        computed: {
+            filteredFruits() {
+                return this.fruits.filter((element) => {
+                    return element.match(this.filterText);
+                });
             }
         }
     }
