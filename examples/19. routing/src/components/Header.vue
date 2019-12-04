8d8b4445f8a81821c84ab9970c04b12e7242e468
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-12-04 19:00:48 +0300

    Section16: navigation

---
+++ examples/19. routing/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <ul class="nav nav-pills">
+        <!--
+        <li role="presentation"><router-link to="/">Home</router-link></li>
+        <li role="presentation"><router-link to="/user">User</router-link></li>
+        -->
+        <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link><!-- Without "exact" it will be a nested behaviour -->
+        <router-link to="/user" tag="li" active-class="active" exact><a>User</a></router-link>
+    </ul>
+</template>
