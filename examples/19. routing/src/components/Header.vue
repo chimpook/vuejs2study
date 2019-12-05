758689f58f7af43eb11773bf1eff49e3313c0e46
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-12-05 19:05:32 +0300

    Section167: Nested routes

--- examples/19. routing/src/components/Header.vue
+++ examples/19. routing/src/components/Header.vue
@@ -5,7 +5,10 @@
         <li role="presentation"><router-link to="/user">User</router-link></li>
         -->
         <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link><!-- Without "exact" it will be a nested behaviour -->
+<!--
         <router-link to="/user/1" tag="li" active-class="active" exact><a>User 1</a></router-link>
         <router-link to="/user/2" tag="li" active-class="active" exact><a>User 2</a></router-link>
+-->
+        <router-link to="/user" tag="li" active-class="active" exact><a>User</a></router-link>
     </ul>
 </template>
