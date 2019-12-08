fe1d29e448c2dd75b914090b32be0aca934a13fb
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-08 17:14:02 +0300

    Section17: vuex - start

---
+++ examples/20. vuex/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result :counter="counter"></app-result>
+        <hr>
+        <app-counter @update="counter += $event"></app-counter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Counter from './components/Counter';
+import Result from './components/Result';
+
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  components: {
+    appCounter: Counter,
+    appResult: Result
+  }
+}
+</script>
---
+++ examples/20. vuex/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="increment">Increment</button>
+        <button class="btn btn-primary" @click="decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        increment() {
+            this.$emit('update', 1);
+        },
+        decrement() {
+            this.$emit('update', -1);
+        }
+    }
+}
+</script>
+
+<style scoped>
+button+button {
+    margin-left: 12px;
+}
+</style>
