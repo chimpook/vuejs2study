7f9f5b1f75338b87d2b936e81b185d777080851a
Сергей Веселовский <s.veselovskiy@estp.ru>
2019-12-09 18:30:07 +0300

    Section17: vuex - getters and spread operator

--- examples/20. vuex/src/components/AnotherResult.vue
+++ examples/20. vuex/src/components/AnotherResult.vue
@@ -1,14 +1,46 @@
 <template>
+<div>
+    <!--
     <p>Counter is {{ counter }}</p>
+    <p>Number of clicks: {{ clicks }}</p>
+    -->
+    <p>Counter is {{ doubleCounter }}</p>
+    <p>Number of clicks: {{ stringCounter }}</p>
+
+</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
     //props: [ 'counter' ]
+/*
     computed: {
         counter() {
-            return this.$store.state.counter * 2;
+            // return this.$store.state.counter * 3;
+            return this.$store.getters.tripleCounter;
+        },
+        clicks() {
+            return this.$store.getters.stringCounter;
         }
+    }
+*/
+// computed: mapGetters({ propertyName: 'doubleCounter })
+/*
+    computed: mapGetters([
+        'doubleCounter',
+        'stringCounter'
+    ])
+*/
+    computed: {
+        ...mapGetters([
+            'doubleCounter',
+            'stringCounter'
+        ]),
+        ourOwn() {
+            return 'something';
+        } 
     }
 }
 </script>
--- examples/20. vuex/src/store/store.js
+++ examples/20. vuex/src/store/store.js
@@ -6,5 +6,16 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         counter: 0
+    },
+    getters: {
+        doubleCounter: state => {
+            return state.counter * 2;
+        },
+        tripleCounter: state => {
+            return state.counter * 3;
+        },
+        stringCounter: state => {
+            return state.counter + ' Clicks';
+        }
     }
 });
