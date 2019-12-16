ef9f00b697e87107851e73faae486645310c20dd
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-16 19:09:16 +0300

    Section17: 278. Using separate files

--- examples/20. vuex/src/store/store.js
+++ examples/20. vuex/src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import counter from './modules/counter';
+import * as actions from './actions';
+import * as mutations from './mutations';
+import * as getters from './getters';
 
 Vue.use(Vuex);
 
@@ -8,26 +11,33 @@ export const store = new Vuex.Store({
     state: {
         value: 0
     },
+/*
     getters: {
         value: state => {
             return state.value;
         }
     },
+*/
+    getters,
+/*
     mutations: {
         updateValue: (state, payload) => {
             state.value = payload;
         }
     },
-    actions: {
+*/
+    mutations,
+//    actions: {
         /*
         increment: context => {
             context.commit('increment'); 
         }
         */
-        updateValue: ({ commit }, payload) => {
-            commit('updateValue', payload);
-        }
-    },
+//        updateValue: ({ commit }, payload) => {
+//            commit('updateValue', payload);
+//        }
+//    },
+    actions,
     modules: {
         counter
     }
