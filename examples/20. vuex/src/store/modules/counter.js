605daddb12283311572204e72b131a490cd788e2
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-16 18:58:25 +0300

    Section17: 277. Modularizing folder structure

---
+++ examples/20. vuex/src/store/modules/counter.js
@@ -0,0 +1,58 @@
+const state = {
+    counter: 0
+};
+
+const getters = {
+    doubleCounter: state => {
+        return state.counter * 2;
+    },
+    tripleCounter: state => {
+        return state.counter * 3;
+    },
+    stringCounter: state => {
+        return state.counter + ' Clicks';
+    }
+};
+
+const mutations = {
+    increment: (state, payload) => {
+        if (payload) {
+            state.counter += payload;
+        } else {
+            state.counter++;
+        }
+    },
+    decrement: (state, payload) => {
+        if (payload) {
+            state.counter -= payload;
+        } else {
+            state.counter--;
+        }
+    }
+};
+
+const actions = {
+    increment: ({ commit }, payload) => {
+        commit('increment', payload);
+    },
+    decrement: ({ commit }) => {
+        commit('decrement');
+    },
+    asyncIncrement: ({ commit }, payload) => {
+        setTimeout(() => {
+            commit('increment', payload.by);
+        },payload.duration);
+    },
+    asyncDecrement: ({ commit }, payload) => {
+        setTimeout(() => {
+            commit('decrement', payload.by);
+        },payload.duration); 
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+};
--- examples/20. vuex/src/store/store.js
+++ examples/20. vuex/src/store/store.js
@@ -1,42 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import counter from './modules/counter';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0,
         value: 0
     },
     getters: {
-        doubleCounter: state => {
-            return state.counter * 2;
-        },
-        tripleCounter: state => {
-            return state.counter * 3;
-        },
-        stringCounter: state => {
-            return state.counter + ' Clicks';
-        },
         value: state => {
             return state.value;
         }
     },
     mutations: {
-        increment: (state, payload) => {
-            if (payload) {
-                state.counter += payload;
-            } else {
-                state.counter++;
-            }
-        },
-        decrement: (state, payload) => {
-            if (payload) {
-                state.counter -= payload;
-            } else {
-                state.counter--;
-            }
-        },
         updateValue: (state, payload) => {
             state.value = payload;
         }
@@ -47,24 +24,11 @@ export const store = new Vuex.Store({
             context.commit('increment'); 
         }
         */
-        increment: ({ commit }, payload) => {
-            commit('increment', payload);
-        },
-        decrement: ({ commit }) => {
-            commit('decrement');
-        },
-        asyncIncrement: ({ commit }, payload) => {
-            setTimeout(() => {
-                commit('increment', payload.by);
-            },payload.duration);
-        },
-        asyncDecrement: ({ commit }, payload) => {
-            setTimeout(() => {
-                commit('decrement', payload.by);
-            },payload.duration); 
-        },
         updateValue: ({ commit }, payload) => {
             commit('updateValue', payload);
         }
+    },
+    modules: {
+        counter
     }
 });
