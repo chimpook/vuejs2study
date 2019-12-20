e8f54843774324020211ebd6c6ede6bb4cc4e8e7
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-20 16:47:42 +0300

    Project4: init

---
+++ projects/04. stock trader/docs/stock trader app.mm
@@ -0,0 +1,92 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="stock trader app" FOLDED="false" ID="ID_1621128631" CREATED="1576849392109" MODIFIED="1576849504943" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.1">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Menu" POSITION="right" ID="ID_1578792758" CREATED="1576849480484" MODIFIED="1576849482687">
+<edge COLOR="#7c0000"/>
+<node TEXT="Stock Trader" ID="ID_1157717423" CREATED="1576849405245" MODIFIED="1576849491672">
+<node TEXT="png_18317379890865767263.png" ID="ID_1761946571" CREATED="1576849555824" MODIFIED="1576849555824">
+<hook URI="stock%20trader%20app_files/png_18317379890865767263.png" SIZE="0.5154639" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Portfolio" ID="ID_1956941487" CREATED="1576849448452" MODIFIED="1576849491679">
+<node TEXT="png_14982038800755223.png" ID="ID_387731464" CREATED="1576849571144" MODIFIED="1576849571144">
+<hook URI="stock%20trader%20app_files/png_14982038800755223.png" SIZE="0.5154639" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Stocks" ID="ID_966897111" CREATED="1576849452908" MODIFIED="1576849491686">
+<node TEXT="png_16006600998346215843.png" ID="ID_1009439834" CREATED="1576849593273" MODIFIED="1576849593273">
+<hook URI="stock%20trader%20app_files/png_16006600998346215843.png" SIZE="0.51502144" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="End Day" ID="ID_1123594028" CREATED="1576849457507" MODIFIED="1576849491694"/>
+<node TEXT="Save &amp; Load" ID="ID_1650483960" CREATED="1576849464676" MODIFIED="1576849491702">
+<node TEXT="png_11181851975803534964.png" ID="ID_1489725063" CREATED="1576849621093" MODIFIED="1576849621093">
+<hook URI="stock%20trader%20app_files/png_11181851975803534964.png" SIZE="0.5176877" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+</map>
