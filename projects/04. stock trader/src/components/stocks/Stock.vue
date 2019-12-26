8defad2ce3f707ac68b719d60827508d54c5338c
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-26 18:22:00 +0300

    Project4: fix styles

--- projects/04. stock trader/src/components/stocks/Stock.vue
+++ projects/04. stock trader/src/components/stocks/Stock.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="col-sm-6 col-md-4 mb-3">
-        <div class="card mt-3">
+    <div class="col-sm-6 col-md-4 mt-3">
+        <div class="card">
             <div class="card-header text-white bg-success">
                     NAME
                     <small>(Price: PRICE)</small>
             </div>
             <div class="card-body">
-                <div class="input-group mb-3">
+                <div class="input-group">
                     <input type="number" class="form-control" placeholder="Quantity">
                     <div class="input-group-append">
                         <button class="btn btn-outline-secondary" type="button">Buy</button>
