a6710e538dfdb32a193996d0a64686e9767c73de
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-29 00:46:13 +0300

    Project4: add portfolio module

---
+++ projects/04. stock trader/src/store/modules/portfolio.js
@@ -0,0 +1,71 @@
+const state = {
+    funds: 10000,
+    stocks: []
+};
+
+const mutations = {
+    'BUY_STOCK'(state, {stockId, quantity, stockPrice}) {
+
+        let sum = stockPrice * quantity;
+        let act_sum = 0;
+        let act_quantity = 0;
+        if (state.funds > sum) {
+            act_quantity = quantity;
+            act_sum = sum;
+        } else {
+            act_quantity = Math.round(state.funds / stockPrice);
+            act_sum = act_quantity * stockPrice;
+        }
+
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record) {
+            record.quantity += act_quantity;
+        } else {
+            state.stocks.push({
+                id: stockId,
+                quantity: act_quantity
+            });
+        }
+        state.funds -= act_sum;
+    },
+    'SELL_STOCK'(state, {stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+            state.funds += stockPrice * quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+            state.funds += stockPrice * record.quantity;
+        }
+    }
+};
+
+const actions = {
+    sellStock({ commit }, order) {
+        commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+    stockPortfolio (state, getters) {
+        return state.stocks.map( stock => {
+            const record = getters.stocks.find(element => element.id == stock.id);
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        });
+    },
+    funds (state) {
+        return state.funds;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+}
