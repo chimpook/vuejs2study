9c8f9e1e5b67c15c3fe07619812a77ca8295ca04
Sergey Veselovskiy <chimpook@gmail.com>
2019-12-30 22:47:58 +0300

    Project4: 302. Setting up vueResource

--- projects/04. stock trader/src/main.js
+++ projects/04. stock trader/src/main.js
@@ -4,10 +4,14 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import { routes } from './routes';
 import store from './store/store';
+import VueResource from 'vue-resource';
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vuejs-stock-trader-f52fd.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
