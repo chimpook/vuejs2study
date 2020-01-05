53b66c87e9dc082880342b8644e648fcf2aede64
Sergey Veselovskiy <chimpook@gmail.com>
2020-01-05 13:33:05 +0300

    Section21: reactive

---
+++ practice/03. reactive/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <!-- 1) Show a "result" of 'not there yet' as long as "value" is not equal to 37 - you can change "value" with the buttons. Print 'done' once you did it -->
+    <div>
+        <p>Current Value: {{ value }}</p>
+        <button @click="value += 5">Add 5</button>
+        <button @click="value += 1">Add 1</button>
+        <p>{{ result }}</p>
+    </div>
+    <!-- 2) Watch for changes in the "result" and reset the "value" after 5 seconds (hint: setTimeout(..., 5000) -->
+    <div>
+        <input type="text" v-model="timeValue">
+        <p>{{ value }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      value: 0,
+      targetValue: 37,
+      timeValue: 3000
+    }
+  },
+  computed: {
+    result() {
+      return this.value < this.targetValue ? 'not there yet' : 'done';
+    }
+  },
+  watch: {
+    result(value) {
+      if (value == 'done') {
+        setTimeout(() => {this.value = 0;}, this.timeValue);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
